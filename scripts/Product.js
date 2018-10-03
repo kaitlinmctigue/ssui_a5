78b0d0f9326e792113f829795d9114d97d8f2cf6
Kaitlin McTigue <kaitlinmctigue@Kaitlins-MBP.wv.cc.cmu.edu>
2018-10-03 11:58:56 -0400

    Products array being added to products page in plain text

--- scripts/Product.js
+++ scripts/Product.js
@@ -1,14 +1,58 @@
 class Product {
-    constructor(title, desc, price, rating, img) {
-        this.title = title
-        this.desc = desc
-        this.price = price
-        this.rating = rating
-        this.img = img
+    constructor(item) {
+        this.title = item.title
+        this.desc = item.desc
+        this.price = item.price
+        this.rating = item.rating
+        this.img = item.img
     }
 
     buildItem() {
-        console.log("build");
+        console.log(this);
+        // let itemBlock = document.createElement("div")
+        // itemBlock.classList.add("itemBlock")
+        //
+        // let catImage = document.createElement("div")
+        // catImage.classList.add("catImage")
+        // catImage.style.backgroundImage = 'url("images/' + imgSrc + '")'
+        // itemBlock.appendChild(catImage)
+        //
+        // let catTitle = document.createElement("div")
+        // catTitle.textContent = title
+        // catTitle.classList.add("catTitle")
+        // itemBlock.appendChild(catTitle)
+        //
+        // let catDesc = document.createElement("div")
+        // catDesc.textContent = desc
+        // catDesc.classList.add("catDesc")
+        // itemBlock.appendChild(catDesc)
 
+        //starr
+        let product = document.createElement("div");
+        product.classList.add("product");
+
+        let image = document.createElement("div");
+        image.classList.add("product__image");
+        image.style.backgroundImage = this.img;
+        product.appendChild(image);
+
+        let price = document.createElement("div");
+        price.classList.add("product__price");
+        price.innerHTML = this.price;
+        product.appendChild(price);
+
+        let desc = document.createElement("div");
+        desc.classList.add("product__desc");
+        desc.innerHTML = this.desc;
+        product.appendChild(desc);
+
+        let rating = document.createElement("div");
+        rating.classList.add("product__rating");
+        for (let i=0; i<this.rating; i++) {
+            rating.innerHTML += ("X");
+        }
+        product.appendChild(rating);
+
+        return product;
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,139 @@
 products = [
     {
-        title: "title",
-        desc: "desc",
-        price: "price",
-        rating: "rating",
-        img: "img"
+        "title": "The Original Round Robin",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$112.00",
+        "rating": "5",
+        "img": "1"
+    },
+    {
+        "title": "The Original Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "2"
+    },
+    {
+        "title": "Fluffmaster 3000",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$54.99",
+        "rating": "4",
+        "img": "3"
+    },
+    {
+        "title": "Couch Perfection",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "99.00",
+        "rating": "3",
+        "img": "4"
+    },
+    {
+        "title": "Fun Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "5"
+    },
+    {
+        "title": "Fluffmaster 3001",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$59.99",
+        "rating": "5",
+        "img": "6"
+    },
+    {
+        "title": "The Original Round Robin",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$112.00",
+        "rating": "5",
+        "img": "1"
+    },
+    {
+        "title": "The Original Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "2"
+    },
+    {
+        "title": "Fluffmaster 3000",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$54.99",
+        "rating": "4",
+        "img": "3"
+    },
+    {
+        "title": "Couch Perfection",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "99.00",
+        "rating": "3",
+        "img": "4"
+    },
+    {
+        "title": "Fun Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "5"
+    },
+    {
+        "title": "Fluffmaster 3001",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$59.99",
+        "rating": "5",
+        "img": "6"
+    },
+    {
+        "title": "The Original Round Robin",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$112.00",
+        "rating": "5",
+        "img": "1"
+    },
+    {
+        "title": "The Original Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "2"
+    },
+    {
+        "title": "Fluffmaster 3000",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$54.99",
+        "rating": "4",
+        "img": "3"
+    },
+    {
+        "title": "Couch Perfection",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "99.00",
+        "rating": "3",
+        "img": "4"
+    },
+    {
+        "title": "Fun Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "5"
+    },
+    {
+        "title": "Fluffmaster 3001",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$59.99",
+        "rating": "5",
+        "img": "6"
     }
 ];
 
 function loadPage() {
-    console.log("it's alive!!!");
+    let productSection = document.getElementsByClassName("all-products")[0];
 
     products.forEach(item => {
         let product = new Product(item);
-        console.log(product);
+        // console.log(product.buildItem());
+        productSection.appendChild(product.buildItem());
     })
 
 }
