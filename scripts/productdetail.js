011f625a0fb2a8cebe729a722f04d66d452f96e0
Kaitlin McTigue <kaitlinmctigue@Kaitlins-MacBook-Pro.local>
2018-10-07 19:17:13 -0400

    rest of product page

--- scripts/productdetail.js
+++ scripts/productdetail.js
@@ -6,7 +6,50 @@ productData = {
     "img": "1"
 };
 
+relatedProducts = [
+    {
+        "title": "Fluffmaster 3000",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$54.99",
+        "rating": "4",
+        "img": "3"
+    },
+    {
+        "title": "Couch Perfection",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "99.00",
+        "rating": "3",
+        "img": "4"
+    },
+    {
+        "title": "Fun Floor Poof",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$89.99",
+        "rating": "5",
+        "img": "5"
+    },
+    {
+        "title": "Fluffmaster 3001",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium massa vitae augue vehicula posuere. Ut gravida ante at maximus mollis. Ut vel eros dictum, auctor metus in, feugiat nisl. Mauris ac arcu risus. Sed commodo justo magna, at rutrum diam auctor eu!",
+        "price": "$59.99",
+        "rating": "5",
+        "img": "6"
+    }
+];
+
 count = 1;
+
+function loadPage() {
+    let sections = document.getElementsByClassName("related-products");
+
+    for (let section of sections) {
+        for (let product of relatedProducts) {
+            let productObj = new Product(product);
+            let productHTML = productObj.buildThumbnail();
+            section.appendChild(productHTML);
+        }
+    }
+}
 
 function loadProduct() {
     let product = new Product(productData);
@@ -26,7 +69,6 @@ function decrement() {
 }
 
 function changeColor(val) {
-    console.log(val);
     let color = document.getElementById("colorpreview");
 
     switch (val) {
