e660114ce90091c8a5af1e20998d381c9e932d2c
Kaitlin McTigue <kaitlinmctigue@Kaitlins-MacBook-Pro.local>
2018-10-07 18:12:08 -0400

    functionality for colorpicker and incrementer

--- scripts/productdetail.js
+++ scripts/productdetail.js
@@ -6,7 +6,46 @@ productData = {
     "img": "1"
 };
 
+count = 1;
+
 function loadProduct() {
     let product = new Product(productData);
     product.buildPage();
 }
+
+function increment() {
+    let control = document.getElementById("count");
+    ++count;
+    control.innerHTML = count;
+}
+
+function decrement() {
+    let control = document.getElementById("count");
+    if (count > 1) {--count;}
+    control.innerHTML = count;
+}
+
+function changeColor(val) {
+    console.log(val);
+    let color = document.getElementById("colorpreview");
+
+    switch (val) {
+        case "1":
+            color.style.backgroundColor = "#d1ffee";
+            break;
+        case "2":
+            color.style.backgroundColor = "#bb93ff";
+            break;
+        case "3":
+            color.style.backgroundColor = "#ffd25a";
+            break;
+        case "4":
+            color.style.backgroundColor = "#252a36";
+            break;
+    }
+}
+
+function changeFill(val) {
+    console.log(val);
+}
+
